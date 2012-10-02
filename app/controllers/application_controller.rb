70066fa9c8f9abb9d87e83f72717da6bea820d8d
Mateusz Król <kroolikk@gmail.com>
2012-10-02 22:11:43 +0200

    dodana metoda na uniwersalny random

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,20 @@ class ApplicationController < ActionController::Base
 
 
   def prepare_variables
-    @nav_pages = Page.where("label != 'kontakt' AND label != 'o-projekcie'")
+    @nav_pages = Page.where("label != 'kontakt' AND label != 'o-projekcie'")    
   end
 
   def init_right_panel
     @r_events = Event.order("created_at DESC").limit(4)
-    @r_photos = Photo.active.order("RANDOM()").limit(6)
+    
+    @r_photos = random_records(Photo.active, 'photos', 6)
+    # @r_photos = Photo.active.order("RANDOM()").limit(6)
   end
   
+
+  def random_records(s_model, s_table_name, s_limit)
+    rand_ids = s_model.all.collect{|m| m.id}.shuffle[0..s_limit-1]
+    rand_objects = s_model.where("#{s_table_name}.id IN ( #{rand_ids.join(',')} )")
+    return rand_objects
+  end
 end
